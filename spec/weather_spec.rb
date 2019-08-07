f31a9085fca1239438bf65d06bd4f6695111b407
Josh Hill <josh.hill@makers.tech>
2019-08-07 14:40:57 +0100

    Test probability of stormy weather

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,10 @@
 require 'weather'
 
 RSpec.describe Weather do
-  it "is stormy sometimes" do
-    expect(subject.stormy?).to be(true).or be(false)
+  it "is stormy about a third of the time" do
+    results = 1000.times.collect { subject.stormy? }
+
+    ratio = results.count(true).fdiv(results.length)
+    expect(ratio).to be_within(0.1).of(0.3)
   end
 end
